3056bc58ec83ac2b2ba877c722f777c9a0d98a8e
ramanbhumireddy <raman.bhumireddy@gmail.com>
2023-01-08 17:01:36 +0530

    bug fix.

--- src/main/java/com/coder/DefaultCtrl.java
+++ src/main/java/com/coder/DefaultCtrl.java
@@ -1,14 +1,13 @@
 package com.coder;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultCtrl {
 	
 	@GetMapping(value = "/msg")
-	public String getMethodName(@RequestParam String param) {
+	public String getMethodName() {
 		return "Hello";
 	}
 
